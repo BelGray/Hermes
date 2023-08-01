2d3273f95d75115c595bd1cee8d1a7f4f12d246e
BelGray <goldendaniel186@gmail.com>
2023-08-01 19:16:02 +0300

    add log system

---
+++ tools/logger.py
@@ -0,0 +1,22 @@
+from tools.BGConsole import BGC
+
+class Log:
+    @staticmethod
+    def s(tag: str, body: str):
+        BGC.write(f"[{tag}]    {body}", param=BGC.Param.BOLD, color=BGC.Color.GREEN)
+
+    @staticmethod
+    def e(tag: str, body: str):
+        BGC.write(f"[{tag}]    {body}", param=BGC.Param.BOLD, color=BGC.Color.RED)
+
+    @staticmethod
+    def w(tag: str, body: str):
+        BGC.write(f"[{tag}]    {body}", param=BGC.Param.BOLD, color=BGC.Color.MUSTARD)
+
+    @staticmethod
+    def i(tag: str, body: str):
+        BGC.write(f"[{tag}]    {body}", param=BGC.Param.BOLD, color=BGC.Color.BLUE)
+
+    @staticmethod
+    def sql(tag: str, body: str, table_name: str):
+        BGC.write(f"[{tag}][{table_name}]    {body}", param=BGC.Param.BOLD, color=BGC.Color.CYAN)

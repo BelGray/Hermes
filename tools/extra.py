d17d8ba808e4b156bc2fd6e0d88d42fbf81233a8
BelGray <goldendaniel186@gmail.com>
2023-08-03 18:42:17 +0300

    add extra.py

---
+++ tools/extra.py
@@ -0,0 +1,25 @@
+
+class Extra:
+
+    def __init__(self):
+        self.__ext = {}
+
+    def __int__(self):
+        self.__ext = {}
+    async def put(self, key: str, value):
+        self.__ext[key] = value
+
+    async def get(self, key: str, default_value = None):
+        if key in self.__ext:
+            return self.__ext[key]
+        return default_value
+
+    async def fetch(self, key: str, default_value=None):
+        if key in self.__ext:
+            fetched = self.__ext[key]
+            del self.__ext[key]
+            return fetched
+        return default_value
+
+    async def purge(self):
+        self.__ext = {}
